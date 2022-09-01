f2cc1f0c0608b3c3c8d659e090e85b901eb8ed00
Kent Madsen <kent.vejrup.madsen@designermadsen.dk>
2022-09-01 16:57:53 +0000

    wandb works and nvidia cuda

--- code/run.py
+++ code/run.py
@@ -1,7 +1,18 @@
+import wandb
+
+from wandb.keras \
+    import WandbCallback
+
+
 def main():
+    wandb.init(project="codespace", entity="designermadsen")
     print("run.py")
 
-    pass
+    wandb.config = {
+        "learning_rate": 0.001,
+        "epochs": 100,
+        "batch_size": 128
+    }
 
 
 if __name__ == '__main__':
