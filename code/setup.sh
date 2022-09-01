f3341cddfb53409ce2b8c1d2d6431ea552951db0
Kent Madsen <kent.vejrup.madsen@designermadsen.dk>
2022-09-01 16:15:30 +0000

    setup script made

--- code/setup.sh
+++ code/setup.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 python3 -m venv .venv
 source ./.venv/bin/activate
+
+# Installation of packages
+pip install wandb
+
+pip install scikit-learn
+pip install tensorflow
+pip install keras
+pip install opencv-python
+
+
