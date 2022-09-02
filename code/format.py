7ce2cf536edb07dd26e82db729a8ba16e7cc92f8
Kent Madsen <kent.vejrup.madsen@designermadsen.dk>
2022-09-02 15:41:50 +0200

    added renaming of dataset to the code

--- code/format.py
+++ code/format.py
@@ -1,6 +1,42 @@
-def main():
+#!/usr/bin/python
+import os
 
-    pass
+paths = '/home/madsen/codespace/frameworks/keras/dataset/train'
+
+files_found = []
+iterator = 0
+
+
+def traverse(path):
+    global files_found
+
+    for root, dirs, files in os.walk(path):
+        for f in files:
+            current_file = os.path.join(root, f)
+            files_found.append(current_file)
+
+
+def iterate(paths):
+    global iterator
+
+    for path in paths:
+        iterator = iterator + 1
+
+        base = os.path.basename(path)
+        extension = os.path.splitext(base)[1]
+
+        artefact_name = str(iterator) + extension
+        directory_path = os.path.dirname(path)
+        full = os.path.join(directory_path, artefact_name)
+        
+        print( "renamed to: " + full )
+        os.rename(path, full)
+
+
+def main():
+    global paths, files_found
+    traverse(paths)
+    iterate(files_found)
 
 
 if __name__ == '__main__':
